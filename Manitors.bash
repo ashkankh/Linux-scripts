6e372cc234fb1d748eadaa2836b5ed528a2c0a40
ashkan <ashkan1@outlook.com>
2023-02-04 12:46:20 +0330

    add dual manitors config

--- Manitors.bash
+++ Manitors.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-xrandr --output HDMI-0 --off  --output DP-1 --mode 1920x1080 --output DVI-I-1 --off
+xrandr --output HDMI-0 --off  --output DP-1 --mode 1920x1080 --output DVI-I-1 --mode 1920X1200
 audio_ID=$(wpctl status | grep "Built-in Audio Analog Stereo" | sed 's@^[^0-9]*\([0-9]\+\).*@\1@') &&
 wpctl set-default $audio_ID
